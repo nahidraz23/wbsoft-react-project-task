cf9571ec827c39f07b5cede0d8049e384b46137c
nahidraz23 <nzamanraz@gmail.com>
2024-10-23 01:52:16 +0600

    cart page implemented

--- src/Page/Cart/Cart.jsx
+++ src/Page/Cart/Cart.jsx
@@ -7,13 +7,16 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 import { BasicContext } from "../../ContextAPIs/BasicProvider";
 
 const Cart = () => {
-   const {cart} = useContext(BasicContext)
+    const { cart } = useContext(BasicContext);
+    const [quantity, setQuantity] = useState(1)
+    const totalPrice = cart[0].discount_price * quantity;
 
-   console.log(cart)
+    console.log(totalPrice)
+    console.log(cart)
 
     return (
         <div className="m-mt_16px">
-           
+
             <h1 className="text-sm text-start md:text-text_xl lg:py-0 font-bold">
                 Cart
             </h1>
@@ -39,71 +42,86 @@ const Cart = () => {
                             </thead>
 
                             <tbody className="overflow-x-auto ">
-                              
-                                    <tr  className="border-b border-gray-300 overflow-x-auto">
-                                        <td>
-                                            <div className="flex items-center justify-center ">
-                                                <div className="w-[20%] text-center flex items-center justify-center ">
-                                                    <RiDeleteBin5Line
-                                                        className="text-xl hover:text-footer_color cursor-pointer"
-                                                        
-                                                    />
-                                                </div>
-                                                <div className="flex flex-col text-center justify-center items-center py-2  w-[80%]">
-                                                    <div className="mask">
-                                                        <img
-                                                            className="h-[40px] w-[70px]"
-                                                            src=''
-                                                            alt='Course'
-                                                        />
-                                                    </div>
-                                                    <p className="text-[14.4px] px-[7px] text-center flex ">
-                                                       Course name  <span className="hidden lg:flex ">- unit name</span>
-                                                    </p>
-                                                </div>
 
+                                <tr className="border-b border-gray-300 overflow-x-auto">
+                                    <td>
+                                        <div className="flex items-center justify-center ">
+                                            <div className="w-[20%] text-center flex items-center justify-center ">
+                                                <RiDeleteBin5Line
+                                                    className="text-xl hover:text-footer_color cursor-pointer"
+
+                                                />
                                             </div>
-                                        </td>
-                                        <td>
-                                            <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
-                                                discount price
-                                            </p>
-                                        </td>
-                                        <td>
-                                            <div className="flex justify-center">
-                                                <div className="border">
-                                                    <button
-                                                        className="px-4 w-[30px] font-bold font_standard my-1.5"
-                                                        
-                                                    >
-                                                        -
-                                                    </button>
-                                                </div>
-                                                <div className="border-y">
-                                                    <input
-                                                        type="number"
-                                                        className="font-bold w-[30px] lg:w-[60px] font_standard px-2 text-center mx-auto h-full"
-                                                      
+                                            <div className="flex flex-col text-center justify-center items-center py-2  w-[80%]">
+                                                <div className="mask">
+                                                    <img
+                                                        className="h-[40px] w-[70px]"
+                                                        src={cart[0]?.photo}
+                                                        alt='Course'
                                                     />
                                                 </div>
-                                                <div className="border">
-                                                    <button
-                                                        className="px-4 w-[30px] font-bold font_standard my-1.5"
-                                                       
-                                                    >
-                                                        +
-                                                    </button>
-                                                </div>
+                                                <p className="text-[14.4px] px-[7px] text-center flex ">
+                                                    {cart[0]?.course_name}  <span className="hidden lg:flex "></span>
+                                                </p>
                                             </div>
-                                        </td>
-                                        <td>
-                                            <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
-                                               
-                                                discount price * quantity
-                                            </p>
-                                        </td>
-                                    </tr>
-                                
+
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
+                                            {cart[0]?.discount_price}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <div className="flex justify-center">
+                                            <div className="border">
+                                                {
+                                                    quantity > 1
+                                                        ?
+                                                        <button
+                                                            onClick={() => setQuantity(quantity - 1)}
+                                                            className="px-4 w-[30px] font-bold font_standard my-1.5"
+
+                                                        >
+                                                            -
+                                                        </button>
+                                                        : 
+                                                        <button
+                                                            className="px-4 w-[30px] font-bold font_standard my-1.5"
+
+                                                        >
+                                                            -
+                                                        </button>
+                                                }
+                                            </div>
+                                            <div className="border-y">
+                                                {/* <input
+                                                    value={quantity}
+                                                    type="number"
+                                                    
+
+                                                /> */}
+                                                <h1 className="font-bold w-[30px] lg:w-[60px] font_standard px-2 py-2 text-center mx-auto h-full">{quantity}</h1>
+                                            </div>
+                                            <div className="border">
+                                                <button
+                                                    onClick={() => setQuantity(quantity + 1)}
+                                                    className="px-4 w-[30px] font-bold font_standard my-1.5"
+
+                                                >
+                                                    +
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p className="text-[14.4px] font-bold p-[7px] text-black text-center">
+
+                                            {totalPrice}
+                                        </p>
+                                    </td>
+                                </tr>
+
                             </tbody>
                         </table>
                     </div>
@@ -113,12 +131,12 @@ const Cart = () => {
                                 Cart Summary
                             </h2>
                             <div className="py-3 flex justify-between border-b border-gray-300">
-                                <p className="text-black font-bold">Total Price</p>
+                                <p className="text-black font-bold">Total Price: {totalPrice}</p>
                                 <p className="text-black font-bold">
-                                    
+
                                 </p>
                             </div>
-                          
+
                             <Link
                                 to={`/cart/checkout`}
                                 state={"bdt"}
